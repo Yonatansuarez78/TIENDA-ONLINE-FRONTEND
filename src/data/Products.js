c1e8371ea32badffb58381c45e5abf8f720dba40
yonatan78 <yonatansuarez78@gmail.com>
2024-06-07 19:05:45 -0500

    activacion y desactivacion del boton comprar dependiendo de la data.countInStock

--- src/data/Products.js
+++ src/data/Products.js
@@ -28,7 +28,7 @@ const products = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
     price: 929,
-    countInStock: 0,
+    countInStock: 5,
     rating: 3.5,
     numReviews: 3,
   },
--- src/screens/SingleProduct.js
+++ src/screens/SingleProduct.js
@@ -1,90 +1,26 @@
-// import React from "react";
-// import { Link, useParams } from "react-router-dom";
-// import Rating from "../components/homeComponents/Rating";
-// import products from "../data/Products";
-// import Header from "./../components/Header";
-// import { useNavigate } from 'react-router-dom';
-
-// const SingleProduct = () => {
-//   const navigate = useNavigate();
-//   const { id } = useParams();
-
-//   // Buscar el producto en base al ID proporcionado en la ruta
-//   const selectedProduct = products.find((p) => p._id === id);
-
-//   // Verificar si el producto fue encontrado
-//   if (!selectedProduct) {
-//     return (
-//       <>
-//         <Header />
-//         <div className="container single-product">
-//           <p>Error: No se encontró el producto con el ID proporcionado.</p>
-//         </div>
-//       </>
-//     );
-//   }
-
-//   function onclick(){
-//     const data = selectedProduct  
-//     navigate('/shipping');
-//   }
-
-
-//   return (
-//     <>
-//       <Header />
-//       <div className="container">
-//         <div className="section">
-//           <div className="row">
-//             <div className="col-lg-12 col-md-12 article">
-//               <div className="shopcontainer row">
-//                 {/* Mostrar solo el producto seleccionado */}
-//                 <div className="shop col-lg-12 border-product">
-//                   <div className="">
-//                     <div className="shopBack">
-//                       <img src={selectedProduct.image} alt={selectedProduct.name} />
-//                     </div>
-
-//                     <div className="shoptext">
-//                       <p> <Link to={`/products/${selectedProduct._id}`}>
-//                           {selectedProduct.name}
-//                         </Link></p>
-
-//                       <Rating
-//                         value={selectedProduct.rating}
-//                         text={`${selectedProduct.numReviews} reviews`}/>
-//                       <h3>${selectedProduct.price}</h3>
-//                     </div>
-//                   </div>
-//                   <button className="name-button text-white ml-auto" style={{ background: "#1cb803"}} onClick={onclick}>Comprar</button>
-
-//                 </div>
-//                 {/* Fin de la sección del producto seleccionado */}
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default SingleProduct;
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import Rating from "../components/homeComponents/Rating";
 import products from "../data/Products";
 import Header from "./../components/Header";
-import { useAuth } from "../context/AuthContext"; // Asegúrate de importar el contexto de autenticación
+import { useAuth } from "../context/AuthContext"; 
 
 const SingleProduct = () => {
-  const { user } = useAuth(); // Obtén el usuario del contexto de autenticación
+  const [countInStock, setCountInStock] = useState(1); // state de productos en countInStock
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); //state desactivar button
+
+  const { user } = useAuth();
   const navigate = useNavigate();
   const { id } = useParams();
 
   // Buscar el producto en base al ID proporcionado en la ruta
   const selectedProduct = products.find((p) => p._id === id);
+
+  useEffect(() => {
+    if (selectedProduct.countInStock == 0)
+    setIsButtonDisabled(true);
+  }, []);
+
 
   // Verificar si el producto fue encontrado
   if (!selectedProduct) {
@@ -98,13 +34,24 @@ const SingleProduct = () => {
     );
   }
 
+
   const handleClick = () => {
-    // Verificar si el usuario está autenticado
-    if (!user) {
+    if (!user) {    // Verificar si el usuario está autenticado
       navigate('/login'); // Redirigir al usuario a la página de inicio de sesión
-      return null; // No renderizar nada mientras se redirige
+      return null;
+    } else {
+      navigate('/shipping');
     }
-    navigate('/shipping');
+  };
+
+  // Crea una lista de opciones basada en countInStock
+  const options = selectedProduct
+    ? Array.from({ length: selectedProduct.countInStock }, (_, i) => i + 1)
+    : [];
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value, 10);
+    setCountInStock(value);
   };
 
   return (
@@ -128,18 +75,34 @@ const SingleProduct = () => {
                           {selectedProduct.name}
                         </Link>
                       </p>
+                      <p>countInStock {selectedProduct.countInStock}</p>
 
                       <Rating
                         value={selectedProduct.rating}
-                        text={`${selectedProduct.numReviews} reviews`}
-                      />
+                        text={`${selectedProduct.numReviews} reviews`} />
+
                       <h3>${selectedProduct.price}</h3>
                     </div>
                   </div>
+                  <select
+                    id="quantity"
+                    value={countInStock}
+                    onChange={handleChange}
+                    className="quantity-select"
+                    disabled={!selectedProduct}
+                  >
+                    {options.map((option) => (
+                      <option key={option} value={option}>{option}</option>
+                    ))}
+                  </select>
                   <button
                     className="name-button text-white ml-auto"
-                    style={{ background: "#1cb803" }}
-                    onClick={handleClick} >
+                    style={{
+                      background: isButtonDisabled ? "#CCCCCC" : "#1cb803", // Cambiar color del botón si está deshabilitado
+                      opacity: isButtonDisabled ? 0.5 : 1 // Cambiar opacidad del botón si está deshabilitado
+                    }}
+                    onClick={handleClick}
+                    disabled={isButtonDisabled} >
                     Comprar
                   </button>
                 </div>
