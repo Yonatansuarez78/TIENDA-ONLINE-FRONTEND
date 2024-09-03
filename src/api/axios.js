8e751e9a07082253f4aaf603ca5c458ec1922f7a
yonatan78 <yonatansuarez78@gmail.com>
2024-09-02 20:54:07 -0500

    cambio 1 - preparacion api para produccion 1.1

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance =  axios.create({
     // baseURL: 'http://localhost:4000/api',
-  baseURL: 'https://whispering-beyond-09111-acac443e4a38.herokuapp.com/api',
+  baseURL: 'https://onlinestore-6b35bdf62a93.herokuapp.com/api',
     withCredentials: true,
       headers: {
         'Content-Type': 'application/json',
